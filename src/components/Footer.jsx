2a4087d1028e283500a5e66d37a77bf8038d9080
uStein <Supannakot013@gmail.com>
2024-10-08 01:48:01 +0700

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Logo from "../img/Leaf.png"
+
+const Footer = () => {
+  return (
+    <footer>
+      <img src={Logo} alt="" />
+      <span>Made with <b>React.js</b></span>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/context/authContext.jsx
@@ -0,0 +1,30 @@
+import { createContext, useEffect, useState } from "react"
+import axios from "axios"
+
+// Set globally
+axios.defaults.withCredentials = true;
+
+export const AuthContext = createContext()
+
+export const AuthContextProvider = ({children})=>{
+    const [currentUser, setCurrentUser] = useState(JSON.parse(localStorage.getItem("user")) || null )
+
+    const login =  async(inputs)=>{
+      const res = await axios.post("http://localhost:8800/api/auth/login", inputs, {withCredentials: true},
+        )
+      setCurrentUser(res.data)
+    }
+
+    const logout =  async(inputs)=>{
+      await axios.post("http://localhost:8800/api/auth/logout", {withCredentials: true})
+      setCurrentUser(null)
+    }
+
+    useEffect(() => {
+        localStorage.setItem("user", JSON.stringify(currentUser))
+    }, [currentUser])
+
+    return (
+        <AuthContext.Provider value={{ currentUser, login, logout }}>{children}</AuthContext.Provider>
+    )
+}
